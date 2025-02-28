7303cd9a12a50296e8fe235640e39983626bef40
simone batista <simoneliveira1224@gmail.com>
2025-02-28 16:26:56 -0300

    atualizando

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const el1 = document.querySelector("#title");
 const el2 = document.querySelector("#info");
+const el3 = document.querySelector(".contact");
 
-const text1 = "Desenvolvedora Full Stack JR | Soluções Web Modernas";
-const text2 = "Transformo ideias em sistemas funcionais, integrando tecnologias front-end e back-end para criar experiências digitais completas. Desenvolvi um mini site funcional com algumas funcionalidades básicas para demonstrar habilidades práticas em HTML, CSS, JavaScript, Bootstrap, PHP, SQL e integração com banco de dados. Para Demonstrar Competências Técnicas, Criar um Portfólio Atraente, Praticar Integração de Tecnologias, Receber Feedback de Especialistas.";
+const text1 = " Olá , meu nome é Simone";
+const text2 = "Sou uma Desenvolvedora FullStack jr! Transformo ideias em sistemas funcionais, integrando tecnologias front-end e back-end para criar experiências digitais completas.";
+const text3 = " <button>Entre em Contato</button>";
 
 const minInterval = 80; // Tempo mínimo entre caracteres
 const maxInterval = 200; // Tempo máximo entre caracteres
@@ -37,7 +39,10 @@ function showText(el, text, callback) {
 showText(el1, text1, () => {
     // Após o primeiro texto ser digitado, exibe o segundo com animação
     el2.innerHTML = text2;
+    
     el2.classList.add("slide-in"); // Adiciona a classe para animar
+    el3.innerHTML = text3;
+    el3.classList.add("slide-in");
 });
 
 
